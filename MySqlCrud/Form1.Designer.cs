fc17a8036fec49ae0f08b66cc2b1c2ca42138b4e
Juanandresfl <juanandresfl@ufps.edu.co>
2020-11-16 17:52:44 -0500

    CRUD

---
+++ MySqlCrud/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿
+namespace MySqlCrud
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Nombre = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btn_Guardar = new System.Windows.Forms.Button();
+            this.libro = new System.Windows.Forms.DataGridView();
+            this.txtAutor = new System.Windows.Forms.TextBox();
+            this.Autor = new System.Windows.Forms.Label();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.Descripcion = new System.Windows.Forms.Label();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.libro)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Nombre
+            // 
+            this.Nombre.AutoSize = true;
+            this.Nombre.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nombre.Location = new System.Drawing.Point(44, 68);
+            this.Nombre.Name = "Nombre";
+            this.Nombre.Size = new System.Drawing.Size(64, 18);
+            this.Nombre.TabIndex = 0;
+            this.Nombre.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.BackColor = System.Drawing.SystemColors.Window;
+            this.txtNombre.Location = new System.Drawing.Point(167, 63);
+            this.txtNombre.Multiline = true;
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(172, 28);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // btn_Guardar
+            // 
+            this.btn_Guardar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btn_Guardar.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Guardar.ForeColor = System.Drawing.Color.White;
+            this.btn_Guardar.Location = new System.Drawing.Point(33, 280);
+            this.btn_Guardar.Name = "btn_Guardar";
+            this.btn_Guardar.Size = new System.Drawing.Size(90, 32);
+            this.btn_Guardar.TabIndex = 2;
+            this.btn_Guardar.Text = "Guardar";
+            this.btn_Guardar.UseVisualStyleBackColor = false;
+            this.btn_Guardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // libro
+            // 
+            this.libro.AllowUserToAddRows = false;
+            this.libro.AllowUserToDeleteRows = false;
+            this.libro.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.libro.Location = new System.Drawing.Point(390, 115);
+            this.libro.Name = "libro";
+            this.libro.ReadOnly = true;
+            this.libro.Size = new System.Drawing.Size(275, 193);
+            this.libro.TabIndex = 3;
+            this.libro.DoubleClick += new System.EventHandler(this.libro_DoubleClick);
+            // 
+            // txtAutor
+            // 
+            this.txtAutor.Location = new System.Drawing.Point(167, 113);
+            this.txtAutor.Multiline = true;
+            this.txtAutor.Name = "txtAutor";
+            this.txtAutor.Size = new System.Drawing.Size(172, 30);
+            this.txtAutor.TabIndex = 5;
+            // 
+            // Autor
+            // 
+            this.Autor.AutoSize = true;
+            this.Autor.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Autor.Location = new System.Drawing.Point(44, 115);
+            this.Autor.Name = "Autor";
+            this.Autor.Size = new System.Drawing.Size(46, 18);
+            this.Autor.TabIndex = 4;
+            this.Autor.Text = "Autor";
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(167, 170);
+            this.txtDescripcion.Multiline = true;
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(172, 79);
+            this.txtDescripcion.TabIndex = 7;
+            // 
+            // Descripcion
+            // 
+            this.Descripcion.AutoSize = true;
+            this.Descripcion.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Descripcion.Location = new System.Drawing.Point(44, 172);
+            this.Descripcion.Name = "Descripcion";
+            this.Descripcion.Size = new System.Drawing.Size(93, 18);
+            this.Descripcion.TabIndex = 6;
+            this.Descripcion.Text = "Descripción";
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.BackColor = System.Drawing.Color.Red;
+            this.btnEliminar.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.ForeColor = System.Drawing.Color.White;
+            this.btnEliminar.Location = new System.Drawing.Point(152, 280);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(89, 32);
+            this.btnEliminar.TabIndex = 8;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button3.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(269, 280);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(85, 32);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Cancelar";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(390, 63);
+            this.txtBuscar.Multiline = true;
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(183, 28);
+            this.txtBuscar.TabIndex = 10;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnBuscar.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.ForeColor = System.Drawing.Color.White;
+            this.btnBuscar.Location = new System.Drawing.Point(590, 63);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 28);
+            this.btnBuscar.TabIndex = 11;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = false;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(677, 362);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.Descripcion);
+            this.Controls.Add(this.txtAutor);
+            this.Controls.Add(this.Autor);
+            this.Controls.Add(this.libro);
+            this.Controls.Add(this.btn_Guardar);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.Nombre);
+            this.Name = "Form1";
+            this.Text = "CRUD C#";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.libro)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Nombre;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btn_Guardar;
+        private System.Windows.Forms.DataGridView libro;
+        private System.Windows.Forms.TextBox txtAutor;
+        private System.Windows.Forms.Label Autor;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.Label Descripcion;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.Button btnBuscar;
+    }
+}
+
---
+++ MySqlCrud/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace MySqlCrud
+{
+    public partial class Form1 : Form
+    {
+        String conexion = @"Server=localhost;Database=bookdb;Uid=root;Pwd=juancho0618;";
+        int bookID = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection mysqlCon = new MySqlConnection(conexion))
+            {
+                mysqlCon.Open();
+                MySqlCommand mySqlCmd = new MySqlCommand("BookAddOrEdit", mysqlCon);
+                mySqlCmd.CommandType = CommandType.StoredProcedure;
+                mySqlCmd.Parameters.AddWithValue("_BookID", bookID);
+                mySqlCmd.Parameters.AddWithValue("_BookName", txtNombre.Text.Trim());
+                mySqlCmd.Parameters.AddWithValue("_Author", txtAutor.Text.Trim());
+                mySqlCmd.Parameters.AddWithValue("_Description", txtDescripcion.Text.Trim());
+                mySqlCmd.ExecuteNonQuery();
+                MessageBox.Show("Agregado correctamente");
+                clear();
+                GridFill();
+            }
+        }
+
+        void GridFill() {
+            using (MySqlConnection mysqlCon = new MySqlConnection(conexion))
+            {
+                mysqlCon.Open();
+                MySqlDataAdapter sqlDa = new MySqlDataAdapter("BookViewAll", mysqlCon);
+                sqlDa.SelectCommand.CommandType = CommandType.StoredProcedure;
+                DataTable dtblBook = new DataTable();
+                sqlDa.Fill(dtblBook);
+                libro.DataSource = dtblBook;
+                libro.Columns[0].Visible = false;
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            clear(); 
+            GridFill();
+            btnEliminar.Enabled = false;
+        }
+
+        void clear() {
+            txtAutor.Text = txtNombre.Text = txtDescripcion.Text = txtBuscar.Text = "";
+            bookID = 0;
+            btn_Guardar.Text = "Guardar";
+        }
+
+        private void libro_DoubleClick(object sender, EventArgs e)
+        {
+            if (libro.CurrentRow.Index != -1) {
+                txtNombre.Text = libro.CurrentRow.Cells[1].Value.ToString();
+                txtAutor.Text = libro.CurrentRow.Cells[2].Value.ToString();
+                txtDescripcion.Text = libro.CurrentRow.Cells[3].Value.ToString();
+                bookID = Convert.ToInt32(libro.CurrentRow.Cells[0].Value.ToString());
+                btn_Guardar.Text = "Editar";
+                btnEliminar.Enabled = Enabled;
+            }
+        }
+
+        private void btnBuscar_Click_1(object sender, EventArgs e)
+        {
+            using (MySqlConnection mysqlCon = new MySqlConnection(conexion))
+            {
+                mysqlCon.Open();
+                MySqlDataAdapter sqlDa = new MySqlDataAdapter("BookSearchByValue", mysqlCon);
+                sqlDa.SelectCommand.CommandType = CommandType.StoredProcedure;
+                sqlDa.SelectCommand.Parameters.AddWithValue("_SearchValue", txtBuscar.Text);
+                DataTable dtblBook = new DataTable();
+                sqlDa.Fill(dtblBook);
+                libro.DataSource = dtblBook;
+                libro.Columns[0].Visible = false;
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            clear();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Eliminado correctamente "+bookID);
+            using (MySqlConnection mysqlCon = new MySqlConnection(conexion))
+            {
+                mysqlCon.Open();
+                MySqlCommand mySqlCmd = new MySqlCommand("BookDeleteByID", mysqlCon);
+                mySqlCmd.CommandType = CommandType.StoredProcedure;
+                mySqlCmd.Parameters.AddWithValue("_BookID", bookID);
+                mySqlCmd.ExecuteNonQuery();
+                MessageBox.Show("Eliminado correctamente");
+                clear();
+                GridFill();
+            }
+        }
+    }
+}
